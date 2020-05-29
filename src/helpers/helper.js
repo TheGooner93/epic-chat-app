77bf54465699fbb42af5e36e89a552d9e978b06e
Mohammed Ehab <mohammedehab93@gmail.com>
2020-05-29 18:28:46 +0400

    Added console.log

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,5 +1,6 @@
 const getChatServerUrl = () => {
   const localUrl = "http://localhost:5000";
+  console.log(process.env);
   return process.env.CHAT_SERVICE_URL ? process.env.CHAT_SERVICE_URL : localUrl;
 };
 
